6733b0c8fc4ec70e85f77fad17ac37b161e7c337
doulongchao <doulongchao@gmail.com>
2018-04-15 21:06:21 +0800

    test

---
+++ src/xxxx.java
@@ -0,0 +1,14 @@
+/**
+ * Created by dou lc on 2018/4/15.
+ */
+public class xxxx {
+    public static  void main(String[] args) {
+        Solution solution2 =new Solution();
+        int [] num={1,1,2,3,3,4};
+        System.out.println(solution2.removeDuplicates(num));
+
+        int [] num2={3,1,2,3,3,4};
+        System.out.println(solution2.removeElement(num2,3));
+    }
+
+}
